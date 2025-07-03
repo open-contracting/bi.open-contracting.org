701e9322163c3226db0b0212ec26807d5482be46
James McKinney <26463+jpmckinney@users.noreply.github.com>
2025-07-03 10:10:55 -0400

    chore: Run ruff check
    - Use keyword arguments for multi-parameter function
    - Move code that cannot raise exception outside try-except block
    - Use consistent order for instance attributes
    - Avoid single-use variable

--- mexico_nuevo_leon_etl/etl.py
+++ mexico_nuevo_leon_etl/etl.py
@@ -1,22 +1,14 @@
 import json
 import os
+
 import psycopg2.sql
 from pymongo import MongoClient
-
-COLLECTIONS_TO_LOAD = [
-    "contests",
-    "db_sheet_plan_anual",
-    "provider_contests",
-    "provider_data",
-    "sheet_contests",
-    "sheet_dependencias",
-]
 
 TARGET_TABLE_NAME_PREFIX = "mexico_nuevo_leon"
 
 
 class DataLoader:
-    def __init__(self, source_database_url, source_database_name, target_database_url):
+    def __init__(self, *, source_database_url, source_database_name, target_database_url):
         self.source_database_url = source_database_url
         self.target_database_url = target_database_url
         self.source_database_name = source_database_name
@@ -26,14 +18,14 @@ class DataLoader:
         self.cursor = None
 
     def connect(self):
+        self.source_database = MongoClient(self.source_database_url)[self.source_database_name]
         self.target_database_connection = psycopg2.connect(self.target_database_url)
         self.cursor = self.target_database_connection.cursor()
 
-        self.source_database = MongoClient(self.source_database_url)[self.source_database_name]
-
     def save_to_target(self, table_name):
+        table_name = f"{TARGET_TABLE_NAME_PREFIX}_{table_name}"
+
         try:
-            table_name = f"{TARGET_TABLE_NAME_PREFIX}_{table_name}"
             self.connect()
             self.cursor.execute(f"DROP TABLE IF EXISTS {table_name}")
             self.cursor.execute(f"CREATE TABLE IF NOT EXISTS {table_name} (data jsonb)")
@@ -47,11 +39,20 @@ class DataLoader:
 
 def main():
     dataloader = DataLoader(
-        os.getenv("NUEVO_LEON_SOURCE_DB_URL", "mongodb://root:example@localhost:27017"),
-        os.getenv("NUEVO_LEON_SOURCE_DB_NAME", "nuevo_leon"),
-        os.getenv("NUEVO_LEON_TARGET_DB_URL", "postgresql://postgres:postgres@localhost:5432/postgres"),
+        source_database_url=os.getenv("NUEVO_LEON_SOURCE_DB_URL", "mongodb://root:example@localhost:27017"),
+        source_database_name=os.getenv("NUEVO_LEON_SOURCE_DB_NAME", "nuevo_leon"),
+        target_database_url=os.getenv(
+            "NUEVO_LEON_TARGET_DB_URL", "postgresql://postgres:postgres@localhost:5432/postgres"
+        ),
     )
-    for collection in COLLECTIONS_TO_LOAD:
+    for collection in (
+        "contests",
+        "db_sheet_plan_anual",
+        "provider_contests",
+        "provider_data",
+        "sheet_contests",
+        "sheet_dependencias",
+    ):
         dataloader.save_to_target(collection)
 
 
