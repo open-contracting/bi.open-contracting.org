42d5b4e63d9ed5f7595085327db7d45fc2b434e6
James McKinney <26463+jpmckinney@users.noreply.github.com>
2025-07-03 10:13:56 -0400

    chore: Use with statements to auto-commit the transaction, and auto-close the cursor

--- mexico_nuevo_leon_etl/etl.py
+++ mexico_nuevo_leon_etl/etl.py
@@ -15,25 +15,22 @@ class DataLoader:
 
         self.source_database = None
         self.target_database_connection = None
-        self.cursor = None
 
     def connect(self):
         self.source_database = MongoClient(self.source_database_url)[self.source_database_name]
         self.target_database_connection = psycopg2.connect(self.target_database_url)
-        self.cursor = self.target_database_connection.cursor()
 
     def save_to_target(self, collection_name):
         table_name = f"{TARGET_TABLE_NAME_PREFIX}_{collection_name}"
 
         try:
             self.connect()
-            self.cursor.execute(f"DROP TABLE IF EXISTS {table_name}")
-            self.cursor.execute(f"CREATE TABLE IF NOT EXISTS {table_name} (data jsonb)")
-            for item in self.source_database[collection_name].find({}, {"_id": False}):
-                self.cursor.execute(f"insert into {table_name} (data) values (%s)", (json.dumps(item, default=str),))
-            self.target_database_connection.commit()
+            with self.target_database_connection, self.target_database_connection.cursor() as cursor:
+                cursor.execute(f"DROP TABLE IF EXISTS {table_name}")
+                cursor.execute(f"CREATE TABLE IF NOT EXISTS {table_name} (data jsonb)")
+                for item in self.source_database[collection_name].find({}, {"_id": False}):
+                    self.cursor.execute(f"insert into {table_name} (data) values (%s)", (json.dumps(item, default=str),))
         finally:
-            self.cursor.close()
             self.target_database_connection.close()
 
 
