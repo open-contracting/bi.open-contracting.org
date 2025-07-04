37dfc2b3f6a76cf04def33ab9b1f4b739df459ff
Yohanna Lisnichuk <yohanitalisnichuk@gmail.com>
2025-07-04 16:02:53 -0300

    fix: run and fix ruff warnings

--- mexico_nuevo_leon_etl/etl.py
+++ mexico_nuevo_leon_etl/etl.py
@@ -1,16 +1,16 @@
 import json
 import os
-import tempfile
 from pathlib import Path
 
 import ijson
 import psycopg2
-import requests as requests
-from psycopg2 import extras, sql
+import requests
 from ocdskit.combine import merge
+from psycopg2 import extras, sql
 from pymongo import MongoClient
 
 TARGET_TABLE_NAME_PREFIX = "mexico_nuevo_leon"
+
 
 class Json(extras.Json):
     def dumps(self, obj):
@@ -37,27 +37,31 @@ class DataLoader:
         self.target_database_connection = psycopg2.connect(self.target_database_url)
 
     def get_list_of_existing_ocds_files(self):
-        return [file for file in os.listdir(self.files_store_path) if file.endswith(".json")]
+        return [file for file in Path.iterdir(self.files_store_path) if file.name.endswith(".json")]
 
     def get_compiled_public_ocds_data(self):
         existing_files = self.get_list_of_existing_ocds_files()
-        response = requests.get('https://catalogodatos.nl.gob.mx/api/3/action/package_show?id=contrataciones-abiertas'
-                                '-direccion-general-de-adquisiciones-y-servicios', verify=False)
-        for resource in response.json()['result']['resources']:
-            resource_file_name = f'{resource["name"]}.json'
-            if resource_file_name.upper().startswith('JSON-OCDS') and resource_file_name not in existing_files:
+        response = requests.get(
+            "https://catalogodatos.nl.gob.mx/api/3/action/package_show?id=contrataciones-abiertas"
+            "-direccion-general-de-adquisiciones-y-servicios",
+            verify=False,  # noqa: S501
+            timeout=10,
+        )
+        for resource in response.json()["result"]["resources"]:
+            resource_file_name = f"{resource['name']}.json"
+            if resource_file_name.upper().startswith("JSON-OCDS") and resource_file_name not in existing_files:
                 file_name = self.files_store_path / resource_file_name
-                json_data = requests.get(resource['url'], verify=False).json()
-                with open(file_name, 'w') as f:
+                json_data = requests.get(resource["url"], verify=False, timeout=180).json()  # noqa: S501
+                with Path.open(file_name, "w") as f:
                     json.dump(json_data, f)
         return merge(self.yield_items_from_directory(self.files_store_path))
 
     def yield_items_from_directory(self, crawl_directory):
         for root, _, files in os.walk(crawl_directory):
             for name in files:
-                if name.endswith('.json'):
-                    with open(os.path.join(root, name), 'rb') as f:
-                        yield from ijson.items(f, 'releases.item')
+                if name.endswith(".json"):
+                    with Path.open(Path(root) / name, "rb") as f:
+                        yield from ijson.items(f, "releases.item")
 
     def save_to_target(self, collection, data=None):
         table = f"{TARGET_TABLE_NAME_PREFIX}_{collection}"
@@ -89,7 +93,7 @@ def main():
         target_database_url=os.getenv(
             "NUEVO_LEON_TARGET_DB_URL", "postgresql://postgres:postgres@localhost:5432/postgres"
         ),
-        files_store_path=os.getenv("NUEVO_LEON_FILES_STORE_PATH", "data")
+        files_store_path=os.getenv("NUEVO_LEON_FILES_STORE_PATH", "data"),
     )
     for collection in (
         "db_sheet_plan_anual",
@@ -99,7 +103,7 @@ def main():
     ):
         dataloader.save_to_target(collection)
 
-    dataloader.save_to_target('ocds_public', dataloader.get_compiled_public_ocds_data())
+    dataloader.save_to_target("ocds_public", dataloader.get_compiled_public_ocds_data())
 
 
 if __name__ == "__main__":
