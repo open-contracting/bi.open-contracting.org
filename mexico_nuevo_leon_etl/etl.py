5cc573b498c172e49291014e3823e593f33a33ed
James McKinney <26463+jpmckinney@users.noreply.github.com>
2025-07-23 18:54:54 -0400

    fix: Move connections outside "try", so that "finally" succeeds. chore: Use json.dumps directly to avoid initializer. Skip unnecessary if-condition.

--- mexico_nuevo_leon_etl/etl.py
+++ mexico_nuevo_leon_etl/etl.py
@@ -15,8 +15,7 @@ TARGET_TABLE_NAME_PREFIX = "mexico_nuevo_leon"
 
 class Json(extras.Json):
     def dumps(self, obj):
-        # The original datasets contains dates
-        return json.dumps(obj, default=str)
+        return json.dumps(obj, default=str)  # pymongo decodes to native datetimes
 
 
 # Adapted from kingfisher-collect/kingfisher_scrapy/extensions/database_store.py
@@ -39,61 +38,60 @@ def update_target_database(connection, collection, data):
 
 
 def main():
+    source_database_connection = MongoClient(
+        os.getenv("NUEVO_LEON_SOURCE_DB_URL", "mongodb://root:example@localhost:27017")
+    )
+    target_database_connection = psycopg2.connect(
+        os.getenv("NUEVO_LEON_TARGET_DB_URL", "postgresql://postgres:postgres@localhost:5432/postgres")
+    )
+
+    update = partial(update_target_database, target_database_connection)
+
     try:
-        source_database_connection = MongoClient(
-            os.getenv("NUEVO_LEON_SOURCE_DB_URL", "mongodb://root:example@localhost:27017")
-        )
-        target_database_connection = psycopg2.connect(
-            os.getenv("NUEVO_LEON_TARGET_DB_URL", "postgresql://postgres:postgres@localhost:5432/postgres")
-        )
         source_database = source_database_connection[os.getenv("NUEVO_LEON_SOURCE_DB_NAME", "nuevo_leon")]
         files_store_path = Path(os.getenv("NUEVO_LEON_FILES_STORE_PATH", "data"))
 
-        update = partial(update_target_database, target_database_connection)
+        for collection in (
+            "db_sheet_plan_anual",
+            "db_provider_data",
+            "sheet_contests",
+            "sheet_dependencias",
+        ):
+            update(collection, source_database[collection].find({}, {"_id": False}))
 
-        try:
-            for collection in (
-                "db_sheet_plan_anual",
-                "db_provider_data",
-                "sheet_contests",
-                "sheet_dependencias",
-            ):
-                update(collection, source_database[collection].find({}, {"_id": False}))
+        # Compile the internal OCDS data, and update the target database.
+        update(
+            "ocds_internal",
+            merge(
+                json.loads(json.dumps(item, default=str))  # pymongo decodes to native datetimes
+                for item in source_database["db_release_ocds_detalle"].find({}, {"_id": False})
+            ),
+        )
 
-            # Compile the internal OCDS data, and update the target database.
-            update(
-                "ocds_internal",
-                merge(
-                    json.loads(Json(item).dumps(item))  # MongoDB decodes to native datetimes
-                    for item in source_database["db_release_ocds_detalle"].find({}, {"_id": False})
-                ),
-            )
+        # Compile the external OCDS data, and update the target database.
+        response = requests.get(
+            "https://catalogodatos.nl.gob.mx/api/3/action/package_show?id=contrataciones-abiertas-direccion-general-de-adquisiciones-y-servicios",
+            verify=False,  # noqa: S501
+            timeout=10,
+        )
+        response.raise_for_status()
 
-            # Compile the external OCDS data, and update the target database.
-            response = requests.get(
-                "https://catalogodatos.nl.gob.mx/api/3/action/package_show?id=contrataciones-abiertas-direccion-general-de-adquisiciones-y-servicios",
-                verify=False,  # noqa: S501
-                timeout=10,
-            )
-            response.raise_for_status()
+        files_store_path.mkdir(parents=True, exist_ok=True)
+        existing = {file.name for file in files_store_path.iterdir()}
 
-            files_store_path.mkdir(parents=True, exist_ok=True)
-            existing = {file.name for file in files_store_path.iterdir() if file.name.endswith(".json")}
+        for resource in response.json()["result"]["resources"]:
+            filename = f"{resource['name']}.json"
+            # Note: Delete old files if the publisher changes any.
+            if filename.upper().startswith("JSON-OCDS") and filename not in existing:
+                response = requests.get(resource["url"], verify=False, timeout=180)  # noqa: S501
+                response.raise_for_status()
+                (files_store_path / filename).write_bytes(response.content)
 
-            for resource in response.json()["result"]["resources"]:
-                filename = f"{resource['name']}.json"
-                # Note: Delete old files if the publisher changes any.
-                if filename.upper().startswith("JSON-OCDS") and filename not in existing:
-                    response = requests.get(resource["url"], verify=False, timeout=180)  # noqa: S501
-                    response.raise_for_status()
-                    (files_store_path / filename).write_bytes(response.content)
-
-            update("ocds_external", merge(yield_items_from_directory(files_store_path)))
-        finally:
-            del update  # prevent use of stale connections, in case this code is modified
+        update("ocds_external", merge(yield_items_from_directory(files_store_path)))
     finally:
         source_database_connection.close()
         target_database_connection.close()
+        del update  # prevent use of stale connections, in case this code is modified
 
 
 if __name__ == "__main__":
