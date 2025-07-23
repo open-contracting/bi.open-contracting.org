5b48b8cb765d1a8398e421569df5e62d094335c9
James McKinney <26463+jpmckinney@users.noreply.github.com>
2025-07-23 18:03:24 -0400

    Code review
    fix:
    - Add raise_for_status() to all HTTP requests.
    - Create an iterable of str, not Path, for existing filenames.
    
    perf:
    - Write files from nl.gob.mx without decoding and re-encoding JSON.
    - Avoid constructing a temporary list to pass to merge().
    - Use a set with the "in" operator.
    
    chore:
    - Remove unnecessary class.
    - Always pass the `data` argument.

--- mexico_nuevo_leon_etl/etl.py
+++ mexico_nuevo_leon_etl/etl.py
@@ -1,5 +1,6 @@
 import json
 import os
+from functools import partial
 from pathlib import Path
 
 import ijson
@@ -18,106 +19,80 @@ class Json(extras.Json):
         return json.dumps(obj, default=str)
 
 
-class DataLoader:
-    def __init__(self, *, source_database_url, source_database_name, target_database_url, files_store_path):
-        self.source_database_url = source_database_url
-        self.target_database_url = target_database_url
-        self.source_database_name = source_database_name
+# Adapted from kingfisher-collect/kingfisher_scrapy/extensions/database_store.py
+def yield_items_from_directory(crawl_directory):
+    for root, _, files in os.walk(crawl_directory):
+        root_path = Path(root)
+        for name in files:
+            if name.endswith(".json"):
+                with (root_path / name).open("rb") as f:
+                    yield from ijson.items(f, "releases.item")
 
-        self.source_database = None
-        self.source_database_client = None
-        self.target_database_connection = None
 
-        self.files_store_path = Path(files_store_path)
-        self.files_store_path.mkdir(parents=True, exist_ok=True)
-
-    def connect(self):
-        self.source_database_client = MongoClient(self.source_database_url)
-        self.source_database = self.source_database_client[self.source_database_name]
-        self.target_database_connection = psycopg2.connect(self.target_database_url)
-
-    def disconnect(self):
-        self.source_database = None
-        self.source_database_client.close()
-        self.target_database_connection.close()
-
-    def get_list_of_existing_ocds_files(self):
-        return [file for file in Path.iterdir(self.files_store_path) if file.name.endswith(".json")]
-
-    def get_compiled_public_ocds_data(self):
-        existing_files = self.get_list_of_existing_ocds_files()
-        response = requests.get(
-            "https://catalogodatos.nl.gob.mx/api/3/action/package_show?id=contrataciones-abiertas"
-            "-direccion-general-de-adquisiciones-y-servicios",
-            verify=False,  # noqa: S501
-            timeout=10,
-        )
-        for resource in response.json()["result"]["resources"]:
-            resource_file_name = f"{resource['name']}.json"
-            if resource_file_name.upper().startswith("JSON-OCDS") and resource_file_name not in existing_files:
-                file_name = self.files_store_path / resource_file_name
-                json_data = requests.get(resource["url"], verify=False, timeout=180).json()  # noqa: S501
-                with Path.open(file_name, "w") as f:
-                    json.dump(json_data, f)
-        return merge(self.yield_items_from_directory(self.files_store_path))
-
-    def yield_items_from_directory(self, crawl_directory):
-        for root, _, files in os.walk(crawl_directory):
-            for name in files:
-                if name.endswith(".json"):
-                    with Path.open(Path(root) / name, "rb") as f:
-                        yield from ijson.items(f, "releases.item")
-
-    def get_compiled_internal_ocds_data(self):
-        try:
-            self.connect()
-            items = [
-                json.loads(Json(item).dumps(item))
-                for item in self.source_database["db_release_ocds_detalle"].find({}, {"_id": False})
-            ]
-            return merge(items)
-        finally:
-            self.disconnect()
-
-    def save_to_target(self, collection, data=None):
-        table = f"{TARGET_TABLE_NAME_PREFIX}_{collection}"
-        try:
-            self.connect()
-            data_to_save = data if data else self.source_database[collection].find({}, {"_id": False})
-            with self.target_database_connection, self.target_database_connection.cursor() as cursor:
-                cursor.execute(sql.SQL("DROP TABLE IF EXISTS {table}").format(table=sql.Identifier(table)))
-                cursor.execute(
-                    sql.SQL("CREATE TABLE IF NOT EXISTS {table} (data jsonb)").format(table=sql.Identifier(table))
-                )
-                statement = sql.SQL("INSERT INTO {table} (data) VALUES %s").format(table=sql.Identifier(table))
-                extras.execute_values(
-                    cursor,
-                    statement.as_string(cursor),
-                    [(Json(item),) for item in data_to_save],
-                )
-        finally:
-            self.disconnect()
+def update_target_database(connection, collection, data):
+    table = f"{TARGET_TABLE_NAME_PREFIX}_{collection}"
+    with connection, connection.cursor() as cursor:
+        cursor.execute(sql.SQL("DROP TABLE IF EXISTS {table}").format(table=sql.Identifier(table)))
+        cursor.execute(sql.SQL("CREATE TABLE IF NOT EXISTS {table} (data jsonb)").format(table=sql.Identifier(table)))
+        statement = sql.SQL("INSERT INTO {table} (data) VALUES %s").format(table=sql.Identifier(table))
+        extras.execute_values(cursor, statement.as_string(cursor), [(Json(item),) for item in data])
 
 
 def main():
-    dataloader = DataLoader(
-        source_database_url=os.getenv("NUEVO_LEON_SOURCE_DB_URL", "mongodb://root:example@localhost:27017"),
-        source_database_name=os.getenv("NUEVO_LEON_SOURCE_DB_NAME", "nuevo_leon"),
-        target_database_url=os.getenv(
-            "NUEVO_LEON_TARGET_DB_URL", "postgresql://postgres:postgres@localhost:5432/postgres"
-        ),
-        files_store_path=os.getenv("NUEVO_LEON_FILES_STORE_PATH", "data"),
-    )
-    for collection in (
-        "db_sheet_plan_anual",
-        "db_provider_data",
-        "sheet_contests",
-        "sheet_dependencias",
-    ):
-        dataloader.save_to_target(collection)
+    try:
+        source_database_connection = MongoClient(
+            os.getenv("NUEVO_LEON_SOURCE_DB_URL", "mongodb://root:example@localhost:27017")
+        )
+        target_database_connection = psycopg2.connect(
+            os.getenv("NUEVO_LEON_TARGET_DB_URL", "postgresql://postgres:postgres@localhost:5432/postgres")
+        )
+        source_database = source_database_connection[os.getenv("NUEVO_LEON_SOURCE_DB_NAME", "nuevo_leon")]
+        files_store_path = Path(os.getenv("NUEVO_LEON_FILES_STORE_PATH", "data"))
 
-    dataloader.save_to_target("ocds_internal", dataloader.get_compiled_internal_ocds_data())
-    dataloader.save_to_target("ocds_external", dataloader.get_compiled_public_ocds_data())
+        update = partial(update_target_database, target_database_connection)
+
+        try:
+            for collection in (
+                "db_sheet_plan_anual",
+                "db_provider_data",
+                "sheet_contests",
+                "sheet_dependencias",
+            ):
+                update(collection, source_database[collection].find({}, {"_id": False}))
+
+            # Compile the internal OCDS data, and update the target database.
+            update(
+                "ocds_internal",
+                merge(
+                    json.loads(Json(item).dumps(item))  # MongoDB decodes to native datetimes
+                    for item in source_database["db_release_ocds_detalle"].find({}, {"_id": False})
+                ),
+            )
+
+            # Compile the external OCDS data, and update the target database.
+            response = requests.get(
+                "https://catalogodatos.nl.gob.mx/api/3/action/package_show?id=contrataciones-abiertas-direccion-general-de-adquisiciones-y-servicios",
+                verify=False,  # noqa: S501
+                timeout=10,
+            )
+            response.raise_for_status()
+
+            files_store_path.mkdir(parents=True, exist_ok=True)
+            existing = {file.name for file in files_store_path.iterdir() if file.name.endswith(".json")}
+
+            for resource in response.json()["result"]["resources"]:
+                filename = f"{resource['name']}.json"
+                if filename.upper().startswith("JSON-OCDS") and filename not in existing:  # Note: Delete old files if the publisher changes any
+                    response = requests.get(resource["url"], verify=False, timeout=180)  # noqa: S501
+                    response.raise_for_status()
+                    (files_store_path / filename).write_bytes(response.content)
+
+            update("ocds_external", merge(yield_items_from_directory(files_store_path)))
+        finally:
+            del update  # prevent use of stale connections, in case this code is modified
+    finally:
+        source_database_connection.close()
+        target_database_connection.close()
 
 
 if __name__ == "__main__":
